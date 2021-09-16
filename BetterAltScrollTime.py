6224517cc197acb43a6ec595095998302b74432d
mohmehdi <m.m.afkhami@outlook.com>
2021-09-16 21:54:46 +0430

    removed extra operation

--- BetterAltScrollTime.py
+++ BetterAltScrollTime.py
@@ -3,75 +3,52 @@ bl_info = {
     "blender": (2, 80, 0),
     "category": "Object",
 }
+#---------------------------------------add-on-info------------------------------------#
+
 
 import bpy
+from bpy.props import *
 import addon_utils
 
 
-class ScrollWheelUpTime(bpy.types.Operator):
+class ScrollWheelTime(bpy.types.Operator):
     """Sets the time in range"""      # Use this as a tooltip for menu items and buttons.
-    bl_idname = "object.scroll_up"        # Unique identifier for buttons and menu items to reference.
-    bl_label = "Better Time Scroll Wheel Up"         # Display name in the interface.
+    bl_idname = "object.scroll"        # Unique identifier for buttons and menu items to reference.
+    bl_label = "Better Time Scroll Wheel"         # Display name in the interface.
     bl_options = {'REGISTER', 'UNDO'}  # Enable undo for the operator.
-
-    def execute(self, context):        # execute() is called when running the operator.
-        
-        
+    direction: IntProperty()
+    
+    def execute(self, context):        # execute() is called when running the operator.    
         # logic
         scn = context.scene 
-        current_frame = context.scene.frame_current
-        current_frame-=1
-        context.scene.frame_set(current_frame)
+        current_frame = scn.frame_current
+        current_frame+=self.direction
+        scn.frame_set(current_frame)
            
-        if current_frame < scn.frame_start:
-            context.scene.frame_set(scn.frame_end)
+        if not scn.frame_start <= current_frame <= scn.frame_end:
+            scn.frame_set(scn.frame_start if self.direction==1 else scn.frame_end)
 
         return {'FINISHED'}            # Lets Blender know the operator finished successfully.
-
-
-
-class ScrollWheelDownTime(bpy.types.Operator):
-    """Sets the time in range"""      # Use this as a tooltip for menu items and buttons.
-    bl_idname = "object.scroll_down"        # Unique identifier for buttons and menu items to reference.
-    bl_label = "Better Time Scroll Wheel Down"         # Display name in the interface.
-    bl_options = {'REGISTER', 'UNDO'}  # Enable undo for the operator.
-
-    def execute(self, context):        # execute() is called when running the operator.
-        
-        # logic
-        scn = context.scene 
-        current_frame = context.scene.frame_current
-        current_frame+=1
-        context.scene.frame_set(current_frame)
-                  
-        if current_frame > scn.frame_end:
-            context.scene.frame_set(scn.frame_start)
-
-        return {'FINISHED'}            # Lets Blender know the operator finished successfully.
-
-
-
-
-
-
 
 
 addon_keymaps = []
 
 def menu_func(self, context):
     self.layout.operator(ScrollWheelUpTime.bl_idname)
-    self.layout.operator(ScrollWheelDownTime.bl_idname)
 
 def register():      
-    bpy.utils.register_class(ScrollWheelUpTime)
-    bpy.utils.register_class(ScrollWheelDownTime)
+    bpy.utils.register_class(ScrollWheelTime)
     # Add the hotkey
     wm = bpy.context.window_manager
     kc = wm.keyconfigs.addon
     if kc:
         km = wm.keyconfigs.addon.keymaps.new(name = "Window",space_type='EMPTY', region_type='WINDOW')
-        km_up = km.keymap_items.new(ScrollWheelUpTime.bl_idname, type='WHEELUPMOUSE', value='PRESS', alt=True)
-        km_down = km.keymap_items.new(ScrollWheelDownTime.bl_idname, type='WHEELDOWNMOUSE', value='PRESS', alt=True)
+        km_up = km.keymap_items.new(ScrollWheelTime.bl_idname, type='WHEELUPMOUSE', value='PRESS', alt=True)
+        km_down = km.keymap_items.new(ScrollWheelTime.bl_idname, type='WHEELDOWNMOUSE', value='PRESS', alt=True)
+        
+        km_up.properties.direction = -1     # setting the default property value for wheel up
+        km_down.properties.direction = +1    # setting the default property value for wheel down
+        
         addon_keymaps.append((km, km_up))
         addon_keymaps.append((km, km_down))
     
@@ -79,7 +56,6 @@ def register():
 
 def unregister():
     bpy.utils.unregister_class(ScrollWheelUpTime)
-    bpy.utils.unregister_class(ScrollWheelDownTime)
     
      # Remove the hotkey
     for km, k in addon_keymaps:
--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # Blender Better Alt Scroll Wheel  
 
-normally when using blender <kbd> Alt </kbd> + <kbd> Wheel Up/Down</kbd>  time cursor will pass the end and start frames in timeline and its a bit anoying for animating something like a walk cycle where you want to see the transition between ending and starting over .  
+normally when using blender <kbd> Alt </kbd> + <kbd> Wheel Up/Down</kbd>  time cursor will pass the end and start frames in timeline and its a bit annoying for animating something like a walk cycle where you want to see the transition between ending and starting over .  
 so if you like to use this shortcut you can use this add-on ( my first add on )  
-its a very basic add on (_i encourage you to look at the script as its a good point to start i think_) im just checking if current frame is passed the end or start setting it to where it needs to be  
-one thing to mention is im setting the hotkeys in registeration function of the add on and it doesnt work(becuaze im setting the time frame and defualt hotkey also tries to do that) until you disable default hotkeys, you can see here how:  
+its a very basic add on (_i encourage you to look at the script as its a good point to start i think_) I'm just checking if current frame is passed the end or start setting it to where it needs to be .   
+one thing to mention is I'm setting the hotkeys in registration function of the add on and it doesn't work until you disable default hotkeys (both my addon  and blender frame offset operations try to set frame cursor and it wont work ), you can see here how to disable default one:  
 
 ## How to Install
 
@@ -18,7 +18,12 @@ now those keys are free to use
 
 from:   
 `` Edit -> Prefrences -> Add-Ons ``  
-6. click **install**  
-7. select `` BetterAltScrollTime.py ``  
-8. enable it  
-9. enjoy not taking your hand your mouse wheel to put the frame cursor somewhere and save a lot of energy  
+1. click **install**  
+2. select `` BetterAltScrollTime.py ``  
+3. enable it  
+4. enjoy not taking your hand your mouse wheel to put the frame cursor somewhere and save a lot of energy  
+
+### useful links : 
+[KeyMaps](https://docs.blender.org/api/blender_python_api_2_76_2/bpy.types.KeyMaps.html?highlight=keymaps.new#bpy.types.KeyMaps.new),
+[Operator](https://docs.blender.org/api/current/bpy.types.Operator.html) ,
+[Property Definitions](https://docs.blender.org/api/current/bpy.props.html)
